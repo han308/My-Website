e302ae78a511b75c20d25f9d044139a9e77e1319
han308 <han308@purdue.edu>
2016-01-06 13:19:56 -0600

    Scroll change

--- scroll.js
+++ scroll.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var sOffset = $(".sidebar").offset().top;
     $(window).scroll(function() {
         var scrollYpos = $(document).scrollTop();
-        if (scrollYpos > sOffset) {
+        if (scrollYpos > sOffset - 60) {
             $(".sidebar").css({
                 'top': '61px',
                 'position': 'fixed'
