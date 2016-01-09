57b290ae43d3b14eed0c37a294c6230ed6ff9923
han308 <han308@purdue.edu>
2016-01-08 18:51:46 -0600

    Updated nav.css and bootstrap library

--- scroll.js
+++ scroll.js
@@ -1,17 +1,38 @@
 $(document).ready(function(){
-    var sOffset = $(".sidebar").offset().top;
-    $(window).scroll(function() {
-        var scrollYpos = $(document).scrollTop();
-        if (scrollYpos > sOffset - 60) {
-            $(".sidebar").css({
-                'top': '61px',
-                'position': 'fixed'
+    checkPosition();
+    function checkPosition() {
+        if ($(window).width() < 1200) {
+            var sOffset = $(".sidebar").offset().top;
+            $(window).scroll(function() {
+                var scrollYpos = $(document).scrollTop();
+                if (scrollYpos > sOffset - 60) {
+                    $(".sidebar").css({
+                        'top': '61px',
+                        'position': 'fixed'
+                    });
+                } else {
+                    $(".sidebar").css({
+                        'top': 'auto',
+                        'position': 'absolute'
+                    });
+                }
             });
-        } else {
-            $(".sidebar").css({
-                'top':'auto',
-                'position': 'absolute'
+        } else if($(window).width()>1200){
+            var sOffset = $(".sidebar").offset().top;
+            $(window).scroll(function() {
+                var scrollYpos = $(document).scrollTop();
+                if (scrollYpos > sOffset - 60) {
+                    $(".sidebar").css({
+                        'top': '61px',
+                        'position': 'fixed'
+                    });
+                } else {
+                    $(".sidebar").css({
+                        'top': 'auto',
+                        'position': 'absolute'
+                    });
+                }
             });
         }
-    });
+    }
 });
